1d52e39dbd33e35f04dab14fe3f000aacf104bbd
Quincy Cheng <quincy.cheng@gmail.com>
2023-03-05 17:39:09 +0800

    Create state.cmd

---
+++ state.cmd
@@ -0,0 +1,5 @@
+@echo off
+set install_dir="C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\"
+%install_dir%\vpncli.exe connect connect.tridentusahealth.com -s state
+pause
+echo on
